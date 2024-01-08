6e3c760aba0f110a0af307e44878dda2c2cfa421
jonascarvalh <olaffic@gmail.com>
2024-01-08 20:45:59 -0300

    add: models 'registration'

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Registration(models.Model):
+    choice = (
+        ('Ex', 'Público Externo'),
+        ('E', 'Estudante'), 
+        ('F', 'Funcionário')
+    )
+
+    ocupation = models.CharField(max_length=2, choices=choice)
+    cpf = models.CharField(max_length=11, unique=True)
+    sig_register = models.CharField(max_length=11, unique=True)
+    is_registered = models.BooleanField()
+
+    user = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
