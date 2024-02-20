ff7456bb0cf9944dd97e56e75748b0e845a7cb8f
jonascarvalh <olaffic@gmail.com>
2024-02-19 22:07:55 -0300

    fix: regular expression for verify password and replace logo in navbar

--- utils/django_forms.py
+++ utils/django_forms.py
@@ -9,9 +9,9 @@ def add_placeholder(field, placeholder_val):
     add_attr(field, 'placeholder', placeholder_val)
 
 def strong_password(password):
-    regex = re.compile(r'^.{8}$')
-
-    if not regex.match(password):
+    regex = r'^.{8,}$'
+    print(re.match(regex, password))
+    if not re.match(regex, password):
         raise ValidationError(
             (
                 'A senha precisa conter pelo menos 8 caracteres.'
