db3474cabedb3b90833e0e470e6d45ef6d93a116
jonascarvalh <olaffic@gmail.com>
2024-02-25 17:08:57 -0300

    add: assessment to admin

--- assessment/admin.py
+++ assessment/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from assessment.models import Assessment
+
 
 # Register your models here.
+@admin.register(Assessment)
+class RegistrationAdmin(admin.ModelAdmin):
+    ...
