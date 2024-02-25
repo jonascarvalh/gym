d0f3c3871433ff7dccdec0bc7d2e21e2f1fe8134
jonascarvalh <olaffic@gmail.com>
2024-02-25 16:26:27 -0300

    add: models to assessment

--- assessment/models.py
+++ assessment/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from enrollment.models import Registration
 
 # Create your models here.
+class Assessment(models.Model):
+    choice_is_evaluated = (
+        ('True', 'Avaliado'),
+        ('False', 'Não Avaliado')
+    )
+
+    name   = models.ForeignKey(Registration, on_delete=models.DO_NOTHING)
+    height = models.FloatField(null=False)
+    weight = models.FloatField(null=False) 
+    imc    = models.FloatField(null=False)
+    is_evaluated = models.CharField(max_length=5, choices=choice_is_evaluated)
+
+    def __str__(self):
+        return f'{self.name}'
